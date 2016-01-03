3a15cb5ca782e13f307ac70df6ea476a522081aa
Jnae Jnae <JnaejnaeProductions@gmail.com>
2016-01-02 16:45:33 -0800

    commit

--- src/main/java/com/example/examplemod/Alphapixel.java
+++ src/main/java/com/example/examplemod/Alphapixel.java
@@ -5,11 +5,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Alphapixel.MODID, version = Alphapixel.VERSION)
+public class Alphapixel
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "alphapixel";
+    public static final String VERSION = "0.0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
