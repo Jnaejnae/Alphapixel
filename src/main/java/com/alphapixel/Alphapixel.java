26374f2f71d5dda0f1ff5eedc77c41d3adb7a47f
Jnae Jnae <JnaejnaeProductions@gmail.com>
2016-01-10 12:36:09 -0800

    refactor package rename mod

--- src/main/java/com/alphapixel/Alphapixel.java
+++ src/main/java/com/alphapixel/Alphapixel.java
@@ -1,9 +1,11 @@
-package com.example.examplemod;
+package com.alphapixel;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Alphapixel.MODID, version = Alphapixel.VERSION)
 public class Alphapixel
@@ -11,10 +13,25 @@ public class Alphapixel
     public static final String MODID = "alphapixel";
     public static final String VERSION = "0.0.1";
     
+	@EventHandler
+	public void init(FMLPreInitializationEvent event)
+	{
+		// some example code
+	    System.out.println("preinit");
+	}
+	
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        System.out.println("init");
     }
+
+	
+	@EventHandler
+	public void init(FMLPostInitializationEvent event)
+	{
+		// some example code
+	    System.out.println("postinit");
+	}		
 }
