673a8eb97009cc3909d3066dd68f27d63c0bf4cd
Jnae Jnae <JnaejnaeProductions@gmail.com>
2016-01-10 13:25:12 -0800

    wool drop not workiing

--- src/main/java/com/alphapixel/DropsEventHandler.java
+++ src/main/java/com/alphapixel/DropsEventHandler.java
@@ -1,5 +1,10 @@
 package com.alphapixel;
 
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -10,7 +15,15 @@ public class DropsEventHandler {
 
 	    public void onEvent(LivingDropsEvent event)
 	    {
-		  System.out.println("cancel drops");
+		  System.out.println("wool drops");
 		  event.drops.clear();
+		  if (event.entity instanceof EntitySheep)
+		  {
+			  Item wool = Item.getItemFromBlock(Blocks.wool);
+			  int fleeceColor = ((EntitySheep)(event.entity)).getFleeceColor().getMetadata();
+			  EntityItem dropItem = new EntityItem(event.entity.worldObj);
+			  dropItem.setEntityItemStack(new ItemStack(wool, 1, fleeceColor));
+			  event.drops.add(dropItem);
+		  }
 	    }  
 }
